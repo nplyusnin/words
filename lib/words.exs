2a2444746a241b6b0e0f4c37c1151ab344e15c27
Nikita Plyusnin <nplyusnin78@gmail.com>
2021-12-06 08:38:39 +0300

    added version with pattern matching

--- lib/words.exs
+++ lib/words.exs
@@ -13,7 +13,6 @@ defmodule Words do
     IO.puts(result)
   end
 
-
   def words_flow do
     stream = File.stream!("./data/lorem")
 
@@ -33,12 +32,36 @@ defmodule Words do
     IO.puts(result)
   end
 
+  def words_flow_pattern_matching do
+    stream = File.stream!("./data/lorem")
+
+    result =
+      stream
+      |> Flow.from_enumerable(max_demand: 20)
+      |> Flow.reduce(fn -> [] end, fn(line, acc) ->
+        words_count = parse(line, 1)
+
+        [words_count | acc]
+      end)
+      |> Enum.reduce(0, &(&1 + &2))
+
+    IO.puts(result)
+  end
+
+  def parse("", acc), do: acc
+  def parse(<<?\s, rest :: binary()>>, acc),
+    do: parse(rest, acc + 1)
+  def parse(<<_ :: utf8(), rest :: binary()>>, acc),
+    do: parse(rest, acc)
+
   def run do
     Benchee.run(%{
       "words" => fn -> words() end,
-      "words_flow" => fn -> words_flow() end
+      "words_flow" => fn -> words_flow() end,
+      "words_flow_pattern_matching" => fn -> words_flow_pattern_matching() end
     })
   end
 end
 
 Words.run()
+# IO.inspect(Words.parse("foo bar baz foo bar baz foo bar baz", 1))
